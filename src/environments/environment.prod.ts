2a11f971d5fdb20f3a6085364106dd877c73fcea
Gerardo Martínez <mtz.juncogerardo@gmail.com>
2022-04-11 09:52:00 -0500

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
-  production: true,
-  endpoint: 'https://shinsekai.mx/server/api/',
-  successUrl: 'https://shinsekai.mx/payment/success',
-  errorUrl: 'https://shinsekai.mx/payment/error'
+  production: false,
+  endpoint: 'http://147.182.244.81/api/',
+  successUrl: 'http://147.182.244.81/success/',
+  errorUrl: 'http://147.182.244.81/payment/error/'
 };
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  endpoint: 'https://localhost:44323/api/',
-  successUrl: 'http://localhost:4200/payment/success/',
-  errorUrl: 'http://localhost:4200/payment/error/'
+  endpoint: 'http://147.182.244.81/api/',
+  successUrl: 'http://147.182.244.81/success/',
+  errorUrl: 'http://147.182.244.81/payment/error/'
 };
 
